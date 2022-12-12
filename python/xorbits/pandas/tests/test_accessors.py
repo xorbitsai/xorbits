4b052d51b1aebbb40227dc8613bcbbff0ecc675b
UranusSeven <109661872+UranusSeven@users.noreply.github.com>
2022-12-12 12:40:12 +0800

    Fix: error when accessors being called on DataRef

--- python/xorbits/pandas/tests/test_accessors.py
+++ python/xorbits/pandas/tests/test_accessors.py
@@ -16,13 +16,43 @@
 import pandas as pd
 
 from ... import pandas as xpd
+from ...core import DataRef
+from .. import Series
 
 
-def test_str_split(setup):
-    s = pd.Series(["A_Str_Series"])
+def test_str_accessor(setup):
+    # xs being a xorbits.pandas.core.Series instance
+    s = pd.Series(["A_Str"])
     xs = xpd.Series(s)
-
+    assert isinstance(xs, Series)
     pd.testing.assert_series_equal(s.str.split("_"), xs.str.split("_").to_pandas())
+
+    # xs being a xorbits.core.data.DataRef instance
+    s = s + "_Series"
+    xs = xs + "_Series"
+    assert not isinstance(xs, Series)
+    assert isinstance(xs, DataRef)
+    pd.testing.assert_series_equal(s.str.split("_"), xs.str.split("_").to_pandas())
+
+
+def test_dt_accessor(setup):
+    import numpy as np
+
+    # xs being a xorbits.pandas.core.Series instance
+    a = np.arange(
+        np.datetime64("2000-01-01"), np.datetime64("2000-01-03"), dtype=np.datetime64
+    )
+    s = pd.Series(a)
+    xs = xpd.Series(a)
+    assert isinstance(xs, Series)
+    pd.testing.assert_series_equal(s.dt.year, xs.dt.year.to_pandas())
+
+    # xs being a xorbits.core.data.DataRef instance
+    s = pd.concat((s, s))
+    xs = xpd.concat((xs, xs))
+    assert not isinstance(xs, Series)
+    assert isinstance(xs, DataRef)
+    pd.testing.assert_series_equal(s.dt.year, xs.dt.year.to_pandas())
 
 
 def test_class_docstring():
