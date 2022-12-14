54b601ffde9648471ce805dcefb1d3158c1b8ec6
qianduoduo0904 <qianduoduo0904@gmail.com>
2022-12-14 16:49:19 +0800

    Add social media links

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -4,42 +4,65 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-project = 'Xorbits'
-copyright = '2022, Xprobe-inc'
-author = 'Xprobe-inc'
+project = "Xorbits"
+copyright = "2022, Xprobe-inc"
+author = "Xprobe-inc"
 
 from xorbits import __version__
+
 release = __version__
 
 # -- General configuration ---------------------------------------------------
 extensions = [
-    'sphinx.ext.mathjax',
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.napoleon',
-    'IPython.sphinxext.ipython_directive',
-    'IPython.sphinxext.ipython_console_highlighting',
-    'matplotlib.sphinxext.plot_directive',
+    "sphinx.ext.mathjax",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "IPython.sphinxext.ipython_directive",
+    "IPython.sphinxext.ipython_console_highlighting",
+    "matplotlib.sphinxext.plot_directive",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 
-
 # -- Options for HTML output -------------------------------------------------
-html_theme = 'pydata_sphinx_theme'
+html_theme = "pydata_sphinx_theme"
 
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 html_theme_options = {
-   "logo": {
-      "image_light": "logo.svg",
-      "image_dark": "logo-white.svg",
-   }
+    "logo": {
+        "image_light": "logo.svg",
+        "image_dark": "logo-white.svg",
+    },
+    "external_links": [
+        {"name": "xorbits.io", "url": "https://xorbits.io"},
+    ],
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/xprobe-inc/xorbits",
+            "icon": "fa-brands fa-github",
+            "type": "fontawesome",
+        },
+        {
+            "name": "Slack",
+            "url": "https://slack.xorbits.io",
+            "icon": "fa-brands fa-slack",
+            "type": "fontawesome",
+        },
+        {
+            "name": "Twitter",
+            "url": "https://twitter.com/xorbitsio",
+            "icon": "fa-brands fa-twitter",
+            "type": "fontawesome",
+        },
+    ],
 }
 
 html_favicon = "_static/favicon.svg"
@@ -53,8 +76,7 @@ def remove_flags_docstring(app, what, name, obj, options, lines):
 
 
 def process_class_docstrings(app, what, name, obj, options, lines):
-    """
-    For those classes for which we use ::
+    """For those classes for which we use ::
 
     :template: autosummary/class_without_autosummary.rst
 
@@ -101,109 +123,109 @@ from sphinx.ext.autosummary import Autosummary  # isort:skip
 
 
 class AccessorDocumenter(MethodDocumenter):
-   """
-   Specialized Documenter subclass for accessors.
-   """
+    """
+    Specialized Documenter subclass for accessors.
+    """
 
-   objtype = "accessor"
-   directivetype = "method"
+    objtype = "accessor"
+    directivetype = "method"
 
-   # lower than MethodDocumenter so this is not chosen for normal methods
-   priority = 0.6
+    # lower than MethodDocumenter so this is not chosen for normal methods
+    priority = 0.6
 
-   def format_signature(self):
-      # this method gives an error/warning for the accessors, therefore
-      # overriding it (accessor has no arguments)
-      return ""
+    def format_signature(self):
+        # this method gives an error/warning for the accessors, therefore
+        # overriding it (accessor has no arguments)
+        return ""
 
 
 class AccessorLevelDocumenter(Documenter):
-   """
-   Specialized Documenter subclass for objects on accessor level (methods,
-   attributes).
-   """
+    """
+    Specialized Documenter subclass for objects on accessor level (methods,
+    attributes).
+    """
 
-   # This is the simple straightforward version
-   # modname is None, base the last elements (eg 'hour')
-   # and path the part before (eg 'Series.dt')
-   # def resolve_name(self, modname, parents, path, base):
-   #     modname = 'pandas'
-   #     mod_cls = path.rstrip('.')
-   #     mod_cls = mod_cls.split('.')
-   #
-   #     return modname, mod_cls + [base]
-   def resolve_name(self, modname, parents, path, base):
-      if modname is None:
-         if path:
-            mod_cls = path.rstrip(".")
-         else:
-            mod_cls = None
-            # if documenting a class-level object without path,
-            # there must be a current class, either from a parent
-            # auto directive ...
-            mod_cls = self.env.temp_data.get("autodoc:class")
-            # ... or from a class directive
-            if mod_cls is None:
-               mod_cls = self.env.temp_data.get("py:class")
-            # ... if still None, there's no way to know
-            if mod_cls is None:
-               return None, []
-         # HACK: this is added in comparison to ClassLevelDocumenter
-         # mod_cls still exists of class.accessor, so an extra
-         # rpartition is needed
-         modname, _, accessor = mod_cls.rpartition(".")
-         modname, _, cls = modname.rpartition(".")
-         parents = [cls, accessor]
-         # if the module name is still missing, get it like above
-         if not modname:
-            modname = self.env.temp_data.get("autodoc:module")
-         if not modname:
-            if sphinx.__version__ > "1.3":
-               modname = self.env.ref_context.get("py:module")
+    # This is the simple straightforward version
+    # modname is None, base the last elements (eg 'hour')
+    # and path the part before (eg 'Series.dt')
+    # def resolve_name(self, modname, parents, path, base):
+    #     modname = 'pandas'
+    #     mod_cls = path.rstrip('.')
+    #     mod_cls = mod_cls.split('.')
+    #
+    #     return modname, mod_cls + [base]
+    def resolve_name(self, modname, parents, path, base):
+        if modname is None:
+            if path:
+                mod_cls = path.rstrip(".")
             else:
-               modname = self.env.temp_data.get("py:module")
-         # ... else, it stays None, which means invalid
-      return modname, parents + [base]
+                mod_cls = None
+                # if documenting a class-level object without path,
+                # there must be a current class, either from a parent
+                # auto directive ...
+                mod_cls = self.env.temp_data.get("autodoc:class")
+                # ... or from a class directive
+                if mod_cls is None:
+                    mod_cls = self.env.temp_data.get("py:class")
+                # ... if still None, there's no way to know
+                if mod_cls is None:
+                    return None, []
+            # HACK: this is added in comparison to ClassLevelDocumenter
+            # mod_cls still exists of class.accessor, so an extra
+            # rpartition is needed
+            modname, _, accessor = mod_cls.rpartition(".")
+            modname, _, cls = modname.rpartition(".")
+            parents = [cls, accessor]
+            # if the module name is still missing, get it like above
+            if not modname:
+                modname = self.env.temp_data.get("autodoc:module")
+            if not modname:
+                if sphinx.__version__ > "1.3":
+                    modname = self.env.ref_context.get("py:module")
+                else:
+                    modname = self.env.temp_data.get("py:module")
+            # ... else, it stays None, which means invalid
+        return modname, parents + [base]
 
 
 class AccessorAttributeDocumenter(AccessorLevelDocumenter, AttributeDocumenter):
-   objtype = "accessorattribute"
-   directivetype = "attribute"
+    objtype = "accessorattribute"
+    directivetype = "attribute"
 
-   # lower than AttributeDocumenter so this is not chosen for normal
-   # attributes
-   priority = 0.6
+    # lower than AttributeDocumenter so this is not chosen for normal
+    # attributes
+    priority = 0.6
 
 
 class AccessorMethodDocumenter(AccessorLevelDocumenter, MethodDocumenter):
-   objtype = "accessormethod"
-   directivetype = "method"
+    objtype = "accessormethod"
+    directivetype = "method"
 
-   # lower than MethodDocumenter so this is not chosen for normal methods
-   priority = 0.6
+    # lower than MethodDocumenter so this is not chosen for normal methods
+    priority = 0.6
 
 
 class AccessorCallableDocumenter(AccessorLevelDocumenter, MethodDocumenter):
-   """
-   This documenter lets us removes .__call__ from the method signature for
-   callable accessors like Series.plot
-   """
+    """
+    This documenter lets us removes .__call__ from the method signature for
+    callable accessors like Series.plot
+    """
 
-   objtype = "accessorcallable"
-   directivetype = "method"
+    objtype = "accessorcallable"
+    directivetype = "method"
 
-   # lower than MethodDocumenter; otherwise the doc build prints warnings
-   priority = 0.5
+    # lower than MethodDocumenter; otherwise the doc build prints warnings
+    priority = 0.5
 
-   def format_name(self):
-      return MethodDocumenter.format_name(self).rstrip(".__call__")
+    def format_name(self):
+        return MethodDocumenter.format_name(self).rstrip(".__call__")
 
 
 def setup(app):
 
-   app.connect("autodoc-process-docstring", remove_flags_docstring)
-   app.connect("autodoc-process-docstring", process_class_docstrings)
-   app.add_autodocumenter(AccessorDocumenter)
-   app.add_autodocumenter(AccessorAttributeDocumenter)
-   app.add_autodocumenter(AccessorMethodDocumenter)
-   app.add_autodocumenter(AccessorCallableDocumenter)
+    app.connect("autodoc-process-docstring", remove_flags_docstring)
+    app.connect("autodoc-process-docstring", process_class_docstrings)
+    app.add_autodocumenter(AccessorDocumenter)
+    app.add_autodocumenter(AccessorAttributeDocumenter)
+    app.add_autodocumenter(AccessorMethodDocumenter)
+    app.add_autodocumenter(AccessorCallableDocumenter)
