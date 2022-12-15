91bcdc6bd6effeb66366560de17c7d19bf647c95
qianduoduo0904 <qianduoduo0904@gmail.com>
2022-12-15 15:34:24 +0800

    isort

--- python/xorbits/deploy/__init__.py
+++ python/xorbits/deploy/__init__.py
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Union, List
+from typing import List, Union
 
-from ..core.adapter import mars_new_session, mars_stop_server
 from .._mars.utils import no_default
+from ..core.adapter import mars_new_session, mars_stop_server
 
 
 def init(
