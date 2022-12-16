7936a42b6ecf07bdee9dc48254f14b14f269d1c6
aresnow <aresnow1@gmail.com>
2022-12-16 16:13:57 +0800

    Add CD for xorbits

---
+++ CI/test_basic_execution.py
@@ -0,0 +1,30 @@
+# Tests for wheel
+import warnings
+
+import numpy
+import pandas
+import requests
+
+
+def test_basic_cases():
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        import xorbits
+        import xorbits.pandas as pd
+        import xorbits.numpy as np
+
+    xorbits.init()
+    df = pd.DataFrame({'A': [1, 2, 3]})
+    assert str(df) == str(pandas.DataFrame({'A': [1, 2, 3]}))
+
+    array = np.ones((2, 2))
+    assert str(array) == str(numpy.ones((2, 2)))
+
+    # make sure web is built
+    from xorbits._mars.deploy.oscar.session import get_default_session
+
+    default_session = get_default_session()
+    web_url = default_session.get_web_endpoint()
+    js_url = f'{web_url}/static/bundle.js'
+    assert requests.get(js_url).status_code == 200
+    default_session.stop_server()
