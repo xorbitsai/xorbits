f47c36c8c9e45340e24d98fcb18d328f110db1a2
aresnow1 <109642806+aresnow1@users.noreply.github.com>
2022-12-16 21:55:13 +0800

    BUG: Remove imports from test module (#112)

--- python/xorbits/core/adapter.py
+++ python/xorbits/core/adapter.py
@@ -65,7 +65,6 @@ from .._mars.dataframe.window.ewm.core import EWM as MarsEWM
 from .._mars.dataframe.window.expanding.core import Expanding as MarsExpanding
 from .._mars.dataframe.window.rolling.core import Rolling as MarsRolling
 from .._mars.deploy.oscar import session
-from .._mars.deploy.oscar.tests.session import new_test_session as new_mars_test_session
 from .._mars.tensor.core import TENSOR_TYPE as MARS_TENSOR_TYPE
 from .._mars.tensor.core import Tensor as MarsTensor
 from .._mars.tensor.core import flatiter as mars_flatiter
