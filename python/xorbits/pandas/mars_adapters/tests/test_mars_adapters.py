1cb00f3d75080145bbfc1874d72e3abefb1dc8d5
huanghaojie <3263781582@qq.com>
2022-12-29 19:00:51 +0800

    move MarsGetAttrProxy to xorbits/core/adapter.py

--- python/xorbits/pandas/mars_adapters/tests/test_mars_adapters.py
+++ python/xorbits/pandas/mars_adapters/tests/test_mars_adapters.py
@@ -16,8 +16,8 @@ import pandas as pd
 
 from .... import pandas as xpd
 from ....core import DataRef
+from ....core.adapter import MarsGetAttrProxy
 from ...indexing import DataFrameAt, DataFrameIat, DataFrameIloc, DataFrameLoc
-from ..core import MarsGetAttrProxy
 
 
 def test_dataframe_categorical(setup):
