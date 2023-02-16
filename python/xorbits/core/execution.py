72e14449831bf4141a60f97b6ebe824a23f3e201
UranusSeven <109661872+UranusSeven@users.noreply.github.com>
2023-02-16 12:59:48 +0800

    ENH: execute refs in user ns eagerly

--- python/xorbits/core/execution.py
+++ python/xorbits/core/execution.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import builtins
 from typing import List, Tuple, Union
 
 from .adapter import MarsEntity, mars_execute
@@ -37,13 +38,15 @@ def run(obj: Union[DataRef, List[DataRef], Tuple[DataRef]], **kwargs) -> None:
     obj : DataRef or collection of DataRefs
         DataRef or collection of DataRefs to execute.
     """
+    refs = set(_collect_user_ns_refs())
+
     if isinstance(obj, DataRef):
-        if need_to_execute(obj):
-            mars_execute(_get_mars_entity(obj), **kwargs)
+        refs.add(obj)
     else:
-        refs_to_execute = [_get_mars_entity(ref) for ref in obj if need_to_execute(ref)]
-        if refs_to_execute:
-            mars_execute(refs_to_execute, **kwargs)
+        refs.union(set(obj))
+
+    mars_tileables = [_get_mars_entity(ref) for ref in refs if need_to_execute(ref)]
+    mars_execute(mars_tileables, **kwargs)
 
 
 def need_to_execute(ref: DataRef):
@@ -51,4 +54,27 @@ def need_to_execute(ref: DataRef):
     return (
         hasattr(mars_entity, "_executed_sessions")
         and len(getattr(mars_entity, "_executed_sessions")) == 0
+    )
+
+
+def _collect_user_ns_refs():
+    if not _is_interactive() or not _is_ipython_available():
+        print("not in interactive env or ipython is not available")
+        return []
+
+    ipython = getattr(builtins, "get_ipython")()
+    return [v for k, v in ipython.user_ns.items() if isinstance(v, DataRef)]
+
+
+def _is_interactive():
+    import sys
+
+    # See: https://stackoverflow.com/a/64523765/7098025
+    return hasattr(sys, "ps1")
+
+
+def _is_ipython_available():
+    return (
+        hasattr(builtins, "get_ipython")
+        and getattr(builtins, "get_ipython", None) is not None
     )
