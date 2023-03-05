f6758c913d865df7590570a23f2e9566c3adf461
RandomY-2 <69881587+RandomY-2@users.noreply.github.com>
2023-03-04 20:27:00 -0800

    Update python/xorbits/numpy/numpy_adapters/tests/test_numpy_adapters.py
    Co-authored-by: aresnow1 <109642806+aresnow1@users.noreply.github.com>
    Signed-off-by: RandomY-2 <69881587+RandomY-2@users.noreply.github.com>

--- python/xorbits/numpy/numpy_adapters/tests/test_numpy_adapters.py
+++ python/xorbits/numpy/numpy_adapters/tests/test_numpy_adapters.py
@@ -1,5 +1,6 @@
 # Copyright 2022-2023 XProbe Inc.
 # derived from copyright 1999-2021 Alibaba Group Holding Ltd.
+# derived from copyright 1999-2021 Alibaba Group Holding Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
