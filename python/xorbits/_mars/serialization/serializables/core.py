633c69ad66580ba52d3994ca7b7999069bd37e34
aresnow1 <109642806+aresnow1@users.noreply.github.com>
2023-03-30 16:47:51 +0800

    TST: Add "_mars" directory to coverage (#305)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- python/xorbits/_mars/serialization/serializables/core.py
+++ python/xorbits/_mars/serialization/serializables/core.py
@@ -81,14 +81,7 @@ class SerializableMeta(type):
                 properties_without_fields[k] = v
                 continue
 
-            field = all_fields.get(k)
-            if field is None:
-                properties_field_slot_names.append(k)
-            else:
-                v.name = field.name
-                v.get = field.get
-                v.set = field.set
-                v.__delete__ = field.__delete__
+            properties_field_slot_names.append(k)
             all_fields[k] = v
 
         # Make field order deterministic to serialize it as list instead of dict.
