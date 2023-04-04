0badc5fbb4dce4129388192cdeb76b32aa30af2e
RandomY-2 <69881587+RandomY-2@users.noreply.github.com>
2023-04-03 21:42:19 -0700

    Update test.py

--- python/test.py
+++ python/test.py
@@ -1,23 +1,30 @@
-# import time
-# from sklearn.datasets import load_iris
-# from xorbits._mars.learn.glm import LogisticRegression
+import time
+from sklearn.datasets import load_iris
+from xorbits._mars.learn.glm import LogisticRegression
 
-# X, y = load_iris(return_X_y=True)
+X, y = load_iris(return_X_y=True)
 
-# start = time.time()
-# clf = LogisticRegression(random_state=0, max_iter=3).fit(X, y)
-# print(clf.predict(X[:2, :]))
-# print(time.time() - start)
+start = time.time()
+clf = LogisticRegression(random_state=0, max_iter=3).fit(X, y)
+print(clf.predict(X[:2, :]))
+print(time.time() - start)
 
-import xorbits._mars.tensor as mt
-import xorbits._mars.remote as mr
+"""
+[[ 0.16994089  0.0524601  -0.22151287]
+ [ 0.42591557 -0.12899287 -0.29289528]
+ [-0.61054611  0.21199489  0.40086962]
+ [-0.28578304  0.03435177  0.25111683]]
+"""
 
-def test(a):
-    a = a.reshape((1, 5))
-    return a
+# import xorbits._mars.tensor as mt
+# import xorbits._mars.remote as mr
 
-l = mr.ExecutableTuple([mr.spawn(test, args=(mt.ones((5, 1)).to_numpy())) for _ in range(10)]).execute().fetch()
+# def test(a):
+#     a = a.reshape((1, 5))
+#     return a
 
-print(l)
-print(mt.sum(l, axis=0).execute().fetch())
+# l = mr.ExecutableTuple([mr.spawn(test, args=(mt.ones((5, 1)).to_numpy())) for _ in range(10)]).execute().fetch()
+
+# print(l)
+# print(mt.sum(l, axis=0).execute().fetch())
 
