161804c1f48aa8411f6088f5e8d8bff83ac35be1
Jiayi Ni <nijiayi1119@outlook.com>
2023-04-16 19:42:55 -0700

    Update test_accessors.py

--- python/xorbits/pandas/tests/test_accessors.py
+++ python/xorbits/pandas/tests/test_accessors.py
@@ -141,5 +141,6 @@ def test_obj_docstring(setup, dummy_str_series, dummy_dt_series, dummy_df):
         "This docstring was copied from pandas."
     )
 
+
 def test_dir(setup):
     assert pd.__dir__().sort() == xpd.__dir__().sort()
