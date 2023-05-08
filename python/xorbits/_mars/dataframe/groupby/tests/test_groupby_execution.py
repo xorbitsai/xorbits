BUG: groupby same agg funcs leads to wrong results

python/xorbits/_mars/dataframe/groupby/tests/test_groupby_execution.py
@@ -26,7 +26,7 @@ except ImportError:  # pragma: no cover
 from .... import dataframe as md
 from ....config import option_context
 from ....core.operand import OperandStage
-from ....tests.core import assert_groupby_equal, require_cudf
+from ....tests.core import assert_groupby_equal, require_cudf, support_cuda
 from ....utils import arrow_array_to_objects, pd_release_version
 from ...core import DATAFRAME_OR_SERIES_TYPE
 from ...utils import is_pandas_2
@@ -1593,3 +1593,37 @@ def test_gpu_groupby_size(data_type, chunked, as_index, sort, setup_gpu):
     else:
         actual = actual.sort_index()
         pd.testing.assert_series_equal(expected, actual)
+
+
+@support_cuda
+def test_groupby_agg_on_same_funcs(setup_gpu, gpu):
+    rs = np.random.RandomState(0)
+    df = pd.DataFrame(
+        {
+            "a": rs.choice(["foo", "bar", "bar"], size=100),
+            "b": rs.choice(["foo", "bar", "bar"], size=100),
+            "c": rs.choice(["foo", "bar", "bar"], size=100),
+        },
+    )
+
+    mdf = md.DataFrame(df, chunk_size=34, gpu=gpu)
+
+    def g1(x):
+        return (x == "foo").sum()
+
+    def g2(x):
+        return (x != "bar").sum()
+
+    def g3(x):
+        # same as g2
+        return (x != "bar").sum()
+
+    pd.testing.assert_frame_equal(
+        df.groupby("a", as_index=False).agg((g1, g2, g3)),
+        mdf.groupby("a", as_index=False).agg((g1, g2, g3)).execute().fetch(),
+    )
+
+    pd.testing.assert_frame_equal(
+        df.groupby("a", as_index=False)["b"].agg((g1, g2, g3)),
+        mdf.groupby("a", as_index=False)["b"].agg((g1, g2, g3)).execute().fetch(),
+    )

python/xorbits/_mars/dataframe/reduction/core.py
@@ -896,16 +896,24 @@ class ReductionCompiler:
         return fun
 
     @staticmethod
-    def _build_mock_return_object(func, input_dtype, ndim):
+    def _build_mock_return_object(func, func_name: Optional[str], input_dtype, ndim):
         from ..initializer import DataFrame as MarsDataFrame
         from ..initializer import Series as MarsSeries
 
+        # if function names are not involved and the input functions are exactly the same, the
+        # output objects will have the same keys, which may cause incorrect results.
+        if func_name is None:
+            func_name = ""
+
         if ndim == 1:
-            mock_series = build_empty_series(np.dtype(input_dtype))
+            mock_series = build_empty_series(np.dtype(input_dtype), name=func_name)
             mock_obj = MarsSeries(mock_series)
         else:
             mock_df = build_empty_df(
-                pd.Series([np.dtype(input_dtype)] * 2, index=["A", "B"])
+                pd.Series(
+                    [np.dtype(input_dtype)] * 2,
+                    index=[f"{func_name}_A", f"{func_name}_B"],
+                )
             )
             mock_obj = MarsDataFrame(mock_df)
 
@@ -930,10 +938,10 @@ class ReductionCompiler:
         self._check_function_valid(func)
 
         try:
-            func_ret = self._build_mock_return_object(func, float, ndim=ndim)
+            func_ret = self._build_mock_return_object(func, func_name, float, ndim=ndim)
         except (TypeError, AttributeError):
             # we may encounter lambda x: x.str.cat(...), use an object series to test
-            func_ret = self._build_mock_return_object(func, object, ndim=1)
+            func_ret = self._build_mock_return_object(func, func_name, object, ndim=1)
         output_limit = getattr(func, "output_limit", None) or 1
 
         if not isinstance(func_ret, ENTITY_TYPE):

python/xorbits/_mars/dataframe/reduction/tests/test_reduction_execution.py
@@ -27,7 +27,7 @@ except ImportError:  # pragma: no cover
 from .... import dataframe as md
 from ....config import option_context
 from ....deploy.oscar.session import get_default_session
-from ....tests.core import require_cudf, require_cupy
+from ....tests.core import require_cudf, require_cupy, support_cuda
 from ....utils import lazy_import, pd_release_version
 from ... import CustomReduction, NamedAgg
 from ...base import to_gpu
@@ -1192,3 +1192,31 @@ def test_gpu_multi_agg_methods(setup_gpu):
 
     res = mars_series.agg(["sum", "prod"]).execute().fetch(to_cpu=False)
     pd.testing.assert_series_equal(expected, res.to_pandas())
+
+
+@support_cuda
+def test_agg_on_same_funcs(setup_gpu, gpu):
+    rs = np.random.RandomState(0)
+    df = pd.DataFrame(
+        {
+            "a": rs.choice(["foo", "bar", "bar"], size=100),
+            "b": rs.choice(["foo", "bar", "bar"], size=100),
+            "c": rs.choice(["foo", "bar", "bar"], size=100),
+        },
+    )
+
+    mdf = md.DataFrame(df, chunk_size=34, gpu=gpu)
+
+    def g1(x):
+        return (x == "foo").sum()
+
+    def g2(x):
+        return (x != "bar").sum()
+
+    def g3(x):
+        # same as g2
+        return (x != "bar").sum()
+
+    pd.testing.assert_frame_equal(
+        df.aggregate((g1, g2, g3)), mdf.aggregate((g1, g2, g3)).execute().fetch()
+    )