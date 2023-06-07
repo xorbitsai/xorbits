40528cef5d1bc2732296351f682445edc1d3b0cf
peiyuan <hank010626@163.com>
2023-06-07 14:17:54 +0800

    update

--- python/xorbits/xgboost/mars_adapters/core.py
+++ python/xorbits/xgboost/mars_adapters/core.py
@@ -15,86 +15,89 @@
 import inspect
 from typing import Callable, Dict
 
-import xgboost
+MARS_XGBOOST_CALLABLES = None
 
-from ..._mars.learn.contrib.xgboost.classifier import (
-    XGBClassifier as mars_XGBClassifier,
-)
-from ..._mars.learn.contrib.xgboost.dmatrix import MarsDMatrix
-from ..._mars.learn.contrib.xgboost.regressor import XGBRegressor as mars_XGBRegressor
-from ...core.adapter import mars_xgboost, wrap_mars_callable
+try:
+    import xgboost
+except ImportError:
+    xgboost = None
 
+if xgboost is not None:
+    from ..._mars.learn.contrib.xgboost.classifier import (
+        XGBClassifier as mars_XGBClassifier,
+    )
+    from ..._mars.learn.contrib.xgboost.dmatrix import MarsDMatrix
+    from ..._mars.learn.contrib.xgboost.regressor import (
+        XGBRegressor as mars_XGBRegressor,
+    )
+    from ...core.adapter import mars_xgboost, wrap_mars_callable
 
-class XGBClassifier(mars_XGBClassifier):
-    pass
-
-
-class XGBRegressor(mars_XGBRegressor):
-    pass
-
-
-class DMatrix:
-    def __init__(self) -> None:
+    class XGBClassifier(mars_XGBClassifier):
         pass
 
-    def __call__(self, data, **kws):
-        return MarsDMatrix(data, **kws)
+    class XGBRegressor(mars_XGBRegressor):
+        pass
 
+    class DMatrix:
+        def __init__(self) -> None:
+            pass
 
-def _collect_module_callables() -> Dict[str, Callable]:
-    module_callables: Dict[str, Callable] = dict()
+        def __call__(self, data, **kws):
+            return MarsDMatrix(data, **kws)
 
-    module_callables[xgboost.XGBClassifier.__name__] = XGBClassifier
-    for name, func in inspect.getmembers(XGBClassifier, inspect.isfunction):
-        setattr(
-            XGBClassifier,
-            name,
-            wrap_mars_callable(
-                func,
-                attach_docstring=False,
-                is_cls_member=False,
-                docstring_src_module=xgboost.XGBClassifier,
-                docstring_src=getattr(xgboost.XGBClassifier, name, None),
-            ),
-        )
+    def _collect_module_callables() -> Dict[str, Callable]:
+        module_callables: Dict[str, Callable] = dict()
 
-    module_callables[xgboost.XGBRegressor.__name__] = XGBRegressor
-    for name, func in inspect.getmembers(XGBRegressor, inspect.isfunction):
-        setattr(
-            XGBRegressor,
-            name,
-            wrap_mars_callable(
+        module_callables[xgboost.XGBClassifier.__name__] = XGBClassifier
+        for name, func in inspect.getmembers(XGBClassifier, inspect.isfunction):
+            setattr(
+                XGBClassifier,
+                name,
+                wrap_mars_callable(
+                    func,
+                    attach_docstring=False,
+                    is_cls_member=False,
+                    docstring_src_module=xgboost.XGBClassifier,
+                    docstring_src=getattr(xgboost.XGBClassifier, name, None),
+                ),
+            )
+
+        module_callables[xgboost.XGBRegressor.__name__] = XGBRegressor
+        for name, func in inspect.getmembers(XGBRegressor, inspect.isfunction):
+            setattr(
+                XGBRegressor,
+                name,
+                wrap_mars_callable(
+                    func,
+                    attach_docstring=True,
+                    is_cls_member=False,
+                    docstring_src_module=xgboost.XGBRegressor,
+                    docstring_src=getattr(xgboost.XGBRegressor, name, None),
+                ),
+            )
+
+        module_callables[xgboost.DMatrix.__name__] = DMatrix
+        for name, func in inspect.getmembers(DMatrix, inspect.isfunction):
+            setattr(
+                DMatrix,
+                name,
+                wrap_mars_callable(
+                    func,
+                    attach_docstring=False,
+                    is_cls_member=False,
+                ),
+            )
+
+        for name, func in inspect.getmembers(mars_xgboost, inspect.isfunction):
+            if name == "MarsDMatrix":
+                continue
+            module_callables[name] = wrap_mars_callable(
                 func,
                 attach_docstring=True,
                 is_cls_member=False,
-                docstring_src_module=xgboost.XGBRegressor,
-                docstring_src=getattr(xgboost.XGBRegressor, name, None),
-            ),
-        )
+                docstring_src_module=xgboost,
+                docstring_src=getattr(xgboost, name, None),
+            )
+        return module_callables
 
-    module_callables[xgboost.DMatrix.__name__] = DMatrix
-    for name, func in inspect.getmembers(DMatrix, inspect.isfunction):
-        setattr(
-            DMatrix,
-            name,
-            wrap_mars_callable(
-                func,
-                attach_docstring=False,
-                is_cls_member=False,
-            ),
-        )
-
-    for name, func in inspect.getmembers(mars_xgboost, inspect.isfunction):
-        if name == "MarsDMatrix":
-            continue
-        module_callables[name] = wrap_mars_callable(
-            func,
-            attach_docstring=True,
-            is_cls_member=False,
-            docstring_src_module=xgboost,
-            docstring_src=getattr(xgboost, name, None),
-        )
-    return module_callables
-
-
-MARS_XGBOOST_CALLABLES: Dict[str, Callable] = _collect_module_callables()
+    MARS_XGBOOST_CALLABLES = _collect_module_callables()
