b899fe05d4234a4c4463e8df8aae902adf594977
Rui Ji <ruiji@usc.edu>
2023-06-16 18:03:39 +0800

    first try on implement Len Operands

---
+++ python/xorbits/_mars/dataframe/groupby/len.py
@@ -0,0 +1,98 @@
+import numpy as np
+import pandas as pd
+
+from ... import opcodes
+from ...core import OutputType
+from ...core.operand import MapReduceOperand, OperandStage
+from ..operands import DataFrameOperandMixin, DataFrameShuffleProxy
+
+
+class GroupByLen(DataFrameOperandMixin, MapReduceOperand):
+    _op_type_ = opcodes.GROUPBY_LEN
+
+    def __call__(self, groupby):
+        _in_groupby = (
+            groupby  # the input shall be wrapped-groupby return by the groupby method.
+        )
+
+        return self.new_scalar(self, _in_groupby)
+
+    @classmethod
+    def tile(cls, op: "GroupByLen"):
+        in_groupby = op.inputs[0]
+        output_type = OutputType.series
+
+        # generate map chunks
+        map_chunks = []
+        for chunk in in_groupby.chunks:
+            map_op = op.copy().reset_key()
+            map_op.stage = OperandStage.map
+            map_op._shuffle_size = in_groupby.chunk_shape[0]
+            map_op._output_types = OutputType.series
+            chunk_inputs = [chunk]
+
+            map_chunks.append(map_op.new_chunk(chunk_inputs))
+
+        proxy_chunk = DataFrameShuffleProxy(output_types=[output_type]).new_chunk(
+            map_chunks, shape=()
+        )
+
+        # generate reduce chunks, we only need one reducer here.
+        reduce_chunks = []
+        reduce_op = op.copy().reset_key()
+        reduce_op._output_types = OutputType.scalar
+        reduce_op.stage = OperandStage.reduce
+        reduce_chunks.append(reduce_op.new_chunk([proxy_chunk]))
+
+        # generate the result chunk:
+        out_chunks = []
+        for chunk in reduce_chunks:
+            groupby_len_op = op.copy().reset_key()
+            out_chunks.append(groupby_len_op.new_chunk([chunk]))
+
+        # final wrap up:
+        new_op = op.copy()
+        params = op.outputs[0].params.copy()
+        params["nsplits"] = ((np.nan,) * len(out_chunks),)
+        params["chunks"] = out_chunks
+        return new_op.new_tileables(in_groupby, **params)
+
+    @classmethod
+    def execute_map(cls, ctx, op):
+        chunk = op.outputs[0]
+        in_df_grouped = ctx[op.inputs[0].key]
+
+        # grouped object .size() method ensure every unique keys
+        summary = in_df_grouped.size()
+        sum_indexes = summary.index
+
+        res = []
+        for index in sum_indexes:
+            res.append(index)
+
+        # use series to convey every index store in this level
+        reduce_index = 1
+        ctx[chunk.key, reduce_index] = pd.Series(res)
+
+    @classmethod
+    def execute_reduce(cls, ctx, op: "GroupByLen"):
+        chunk = op.outputs[0]
+        input_idx_to_series = dict(op.iter_mapper_data(ctx))
+        row_idxes = sorted(input_idx_to_series.keys())
+
+        res = set()
+        for row_index in row_idxes:
+            row_series = input_idx_to_series.get(row_index, None)
+            res.update(row_series)
+
+        res_len = len(res)
+        ctx[chunk.key] = res_len
+
+    @classmethod
+    def execute(cls, ctx, op: "GroupByLen"):
+        ctx[op.output[0].key] = ctx[op.inputs[0].key]
+
+
+def groupby_len(groupby):
+    op = GroupByLen()
+    return op(groupby)
