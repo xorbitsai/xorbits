e37fc3439d97c828693aba8fa31ecc98d6d958e7
Yifei Song <79988483+yifeis7@users.noreply.github.com>
2023-06-21 14:09:05 +0800

    FEAT: Add lightGBM support (#507)
    Signed-off-by: Yifei Song <79988483+yifeis7@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ python/xorbits/lightgbm/tests/test_regressor.py
@@ -0,0 +1,89 @@
+# Copyright 2022-2023 XProbe Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pandas as pd
+import pytest
+
+from ... import numpy as xnp
+
+try:
+    import lightgbm
+except ImportError:
+    lightgbm = None
+from ... import lightgbm as lgb
+
+n_rows = 1000
+n_columns = 10
+chunk_size = 200
+rs = xnp.random.RandomState(0)
+X = rs.rand(n_rows, n_columns, chunk_size=chunk_size)
+y = rs.randint(0, 10, n_rows, chunk_size=chunk_size)
+
+
+@pytest.mark.skipif(lightgbm is None, reason="LightGBM not installed")
+def test_local_regressor(setup):
+    regressor = lgb.LGBMRegressor(n_estimators=2)
+    regressor.fit(X, y, verbose=True)
+    prediction = regressor.predict(X)
+
+    assert prediction.ndim == 1
+    assert prediction.shape[0] == len(X)
+
+    assert isinstance(prediction, xnp.ndarray)
+    result = prediction.fetch()
+    assert prediction.dtype == result.dtype
+
+    # test weight
+    weight = xnp.random.rand(X.shape[0])
+    regressor = lgb.LGBMRegressor(verbosity=1, n_estimators=2)
+    regressor.fit(X, y, sample_weight=weight)
+    prediction = regressor.predict(X)
+
+    assert prediction.ndim == 1
+    assert prediction.shape[0] == len(X)
+    result = prediction.fetch()
+    assert prediction.dtype == result.dtype
+
+    # test numpy tensor
+    try:
+        from sklearn.datasets import make_classification
+
+        X_array, y_array = make_classification()
+        regressor = lgb.LGBMRegressor(n_estimators=2)
+        regressor.fit(X_array, y_array, verbose=True)
+        prediction = regressor.predict(X_array)
+
+        assert prediction.ndim == 1
+        assert prediction.shape[0] == len(X_array)
+
+        X_df = pd.DataFrame(X_array)
+        y_df = pd.Series(y_array)
+        regressor = lgb.LGBMRegressor(n_estimators=2)
+        regressor.fit(X_df, y_df, verbose=True)
+        prediction = regressor.predict(X_df)
+
+        assert prediction.ndim == 1
+        assert prediction.shape[0] == len(X_df)
+    except ImportError:
+        pass
+
+    # test existing model
+    X_np = X.execute().fetch()
+    y_np = y.execute().fetch()
+    raw_regressor = lightgbm.LGBMRegressor(verbosity=1, n_estimators=2)
+    raw_regressor.fit(X_np, y_np)
+    prediction = lgb.LGBMRegressor(raw_regressor).predict(X)
+
+    assert prediction.ndim == 1
+    assert prediction.shape[0] == len(X)
