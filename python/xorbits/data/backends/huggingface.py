be4e6ea5d0192230c4872bb300c2649f91c32869
codingl2k1 <codingl2k1@outlook.com>
2023-07-10 16:45:30 +0800

    dev

---
+++ python/xorbits/data/backends/huggingface.py
@@ -0,0 +1,106 @@
+# Copyright 2022-2023 XProbe Inc.
+# derived from copyright 1999-2021 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    Dict,
+    List,
+    Optional,
+    Tuple,
+    TypeVar,
+    Union,
+)
+
+from ..dataset import DatasetData, Dataset
+from ..block import Block
+from ..._mars.core.operand import Operand
+
+
+# class
+
+
+class HuggingfaceDatasetData(DatasetData):
+    def repartition(self, num_blocks: int, **kwargs):
+        pass
+
+
+def from_huggingface(
+    dataset: "datasets.Dataset",
+) -> Union[Dataset, Dict[str, Dataset]]:
+    """Create a dataset from a Hugging Face Datasets Dataset.
+
+    This function is not parallelized, and is intended to be used
+    with Hugging Face Datasets that are loaded into memory (as opposed
+    to memory-mapped).
+
+    Example:
+
+    .. doctest::
+
+        >>> import ray
+        >>> import datasets
+        >>> hf_dataset = datasets.load_dataset("tweet_eval", "emotion")
+        Downloading ...
+        >>> ray_ds = ray.data.from_huggingface(hf_dataset)
+        >>> ray_ds
+        {'train': MaterializedDataset(
+           num_blocks=...,
+           num_rows=3257,
+           schema={text: string, label: int64}
+        ), 'test': MaterializedDataset(
+           num_blocks=...,
+           num_rows=1421,
+           schema={text: string, label: int64}
+        ), 'validation': MaterializedDataset(
+           num_blocks=...,
+           num_rows=374,
+           schema={text: string, label: int64}
+        )}
+        >>> ray_ds = ray.data.from_huggingface(hf_dataset["train"])
+        >>> ray_ds
+        MaterializedDataset(
+           num_blocks=...,
+           num_rows=3257,
+           schema={text: string, label: int64}
+        )
+
+    Args:
+        dataset: A Hugging Face Dataset, or DatasetDict. IterableDataset is not
+            supported. ``IterableDataset`` is not supported.
+
+    Returns:
+        Dataset holding Arrow records from the Hugging Face Dataset, or a dict of
+            datasets in case dataset is a DatasetDict.
+    """
+    import datasets
+
+    def convert(ds: "datasets.Dataset") -> Dataset:
+        # To get the resulting Arrow table from a Hugging Face Dataset after
+        # applying transformations (e.g. train_test_split(), shard(), select()),
+        # we create a copy of the Arrow table, which applies the indices
+        # mapping from the transformations.
+        hf_ds_arrow = ds.with_format("arrow")
+        ray_ds = from_arrow(hf_ds_arrow[:])
+        return ray_ds
+
+    if isinstance(dataset, datasets.Dataset):
+        return convert(dataset)
+    else:
+        raise TypeError(
+            "`dataset` must be a `datasets.Dataset` or `datasets.DatasetDict`."
+            f"got {type(dataset)}"
+        )
---
+++ python/xorbits/data/dataset.py
@@ -0,0 +1,32 @@
+# Copyright 2022-2023 XProbe Inc.
+# derived from copyright 1999-2021 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .._mars.core.entity.objects import Object, ObjectData
+
+
+class DatasetData(ObjectData):
+    def repartition(self, num_blocks: int, **kwargs):
+        raise NotImplementedError
+
+    def map(self, fn, **kwargs):
+        raise NotImplementedError
+
+
+class Dataset(Object):
+    def repartition(self, num_blocks: int, **kwargs):
+        return self.data.repartition(num_blocks, **kwargs)
+
+    def map(self, fn, **kwargs):
+        return self.data.map(fn, **kwargs)
