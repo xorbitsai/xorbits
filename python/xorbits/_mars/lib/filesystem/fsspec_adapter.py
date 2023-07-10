5ebc09214815bf23382ef0eee2cc4839b2feae8e
peiyuan <hank010626@163.com>
2023-07-10 15:54:00 +0800

    BUG: Fix Read Parquet

--- python/xorbits/_mars/lib/filesystem/fsspec_adapter.py
+++ python/xorbits/_mars/lib/filesystem/fsspec_adapter.py
@@ -35,6 +35,8 @@ class FsSpecAdapter(FileSystem):
     def ls(self, path: path_type) -> List[path_type]:
         entries = []
         for entry in self._fs.ls(self._normalize_path(path), detail=False):
+            if entry.strip("/") in path.strip("/"):
+                continue
             if isinstance(entry, Dict):
                 entries.append(entry.get("name"))
             elif isinstance(entry, str):
