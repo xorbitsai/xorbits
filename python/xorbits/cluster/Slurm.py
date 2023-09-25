e61b814e575ba38c9589e9102bebc4a3814a73b9
Aprilies <767760161@qq.com>
2023-09-26 02:56:24 +0800

    fix pre-commit

--- python/xorbits/cluster/Slurm.py
+++ python/xorbits/cluster/Slurm.py
@@ -1,32 +1,35 @@
+import atexit
 import os
+import re
 import subprocess
 import time
-import atexit
+
 import xorbits
 import xorbits.numpy as np
-import re
 
 DEFAULT_JOB_NAME = "default_job"
 DEFAULT_NUMBER_NODES = 2
 DEFAULT_PARTITION_OPTION = "batch"
 DEFAULT_LOAD_ENV = "LOAD_ENV"
 
-class SLURMCluster:
-    def __init__(self,
-                 job_name=None,
-                 num_nodes=None,
-                 partition_option=None,
-                 load_env=None,
-                 output_dir=None,
-                 error_dir=None,
-                 work_dir=None,
-                 time=None,
-                 processes=None,
-                 cores=None,
-                 memory=None,
-                 account=None,
-                 webport=16379):
 
+class SLURMCluster:
+    def __init__(
+        self,
+        job_name=None,
+        num_nodes=None,
+        partition_option=None,
+        load_env=None,
+        output_dir=None,
+        error_dir=None,
+        work_dir=None,
+        time=None,
+        processes=None,
+        cores=None,
+        memory=None,
+        account=None,
+        webport=16379,
+    ):
         commands = ["#!/bin/bash"]
 
         self.job_name = job_name
@@ -61,34 +64,34 @@ class SLURMCluster:
         if self.load_env:
             commands.append(f"source activate {self.load_env}")
 
-        commands +=  [
-                    "set -x",
-                    'nodes=$(scontrol show hostnames "$SLURM_JOB_NODELIST")',
-                    "nodes_array=($nodes)",
-                    "head_node=${nodes_array[0]}",
-                    "port=16380",
-                    f"web_port={self.web_port}",
-                    'echo "Starting SUPERVISOR at ${head_node}"',
-                    'srun --nodes=1 --ntasks=1 -w "${head_node}" \\',
-                    '    xorbits-supervisor -H "${head_node}" -p "${port}" -w "${web_port}"&',
-                    "sleep 30",
-                    'worker_num=$((SLURM_JOB_NUM_NODES - 1))',
-                    'for ((i = 1; i <= worker_num; i++)); do',
-                    '    node_i=${nodes_array[$i]}',
-                    '    port_i=$((port + i))',
-                    '    echo "Starting WORKER $i at ${node_i}"',
-                    '    srun --nodes=1 --ntasks=1 -w "${node_i}" \\',
-                    '        xorbits-worker -H "${node_i}"  -p "${port_i}" -s "${head_node}":"${port}"&',
-                    'done',
-                    "sleep 30",
-                    'address=http://"${head_node}":"${web_port}"',
-                ]
+        commands += [
+            "set -x",
+            'nodes=$(scontrol show hostnames "$SLURM_JOB_NODELIST")',
+            "nodes_array=($nodes)",
+            "head_node=${nodes_array[0]}",
+            "port=16380",
+            f"web_port={self.web_port}",
+            'echo "Starting SUPERVISOR at ${head_node}"',
+            'srun --nodes=1 --ntasks=1 -w "${head_node}" \\',
+            '    xorbits-supervisor -H "${head_node}" -p "${port}" -w "${web_port}"&',
+            "sleep 30",
+            "worker_num=$((SLURM_JOB_NUM_NODES - 1))",
+            "for ((i = 1; i <= worker_num; i++)); do",
+            "    node_i=${nodes_array[$i]}",
+            "    port_i=$((port + i))",
+            '    echo "Starting WORKER $i at ${node_i}"',
+            '    srun --nodes=1 --ntasks=1 -w "${node_i}" \\',
+            '        xorbits-worker -H "${node_i}"  -p "${port_i}" -s "${head_node}":"${port}"&',
+            "done",
+            "sleep 30",
+            'address=http://"${head_node}":"${web_port}"',
+        ]
 
         self.commands = "\n".join(commands)
 
     def run(self):
         shell_commands = self.commands
-        with open("slurm.sh", 'w') as f:
+        with open("slurm.sh", "w") as f:
             f.write(shell_commands)
 
         os.chmod("slurm.sh", 0o770)
@@ -112,7 +115,7 @@ class SLURMCluster:
 
     def get_job_id(self, sbatch_output):
         job_id = None
-        for line in sbatch_output.split('\n'):
+        for line in sbatch_output.split("\n"):
             if "Submitted batch job" in line:
                 job_id = line.split(" ")[-1]
         return job_id
@@ -131,55 +134,65 @@ class SLURMCluster:
                 node_list = None
                 if result.returncode == 0:
                     job_info = result.stdout
-                    node_list_pattern = r'NodeList=(c\[\d+-\d+\]|c\d)'
+                    node_list_pattern = r"NodeList=(c\[\d+-\d+\]|c\d)"
                     matches = re.search(node_list_pattern, job_info)
 
                 if matches:
                     node_list = matches.group(1)
-                    print("NodeList:", node_list)                        
+                    print("NodeList:", node_list)
                     if node_list is None:
-                        raise ValueError(f"Job {self.job_id} not found or NodeList information not available.")
+                        raise ValueError(
+                            f"Job {self.job_id} not found or NodeList information not available."
+                        )
                     # get_head_node from nodelist
                     if "[" in node_list:
-                        head_node = node_list.split("-")[0].replace("[","")
+                        head_node = node_list.split("-")[0].replace("[", "")
                     else:
-                    # when only one node
+                        # when only one node
                         head_node = node_list
 
                     self.head_node = head_node
                     return head_node
                 else:
                     print("NodeList not found in the string.")
-                
+
         except subprocess.CalledProcessError as e:
             print(f"Error executing scontrol: {e}")
         except ValueError as e:
             print(f"Error: {e}")
         except Exception as e:
             print(f"An unexpected error occurred: {e}")
-        
+
         self.head_node = None
         print("Failed to retrieve head node.")
-    
+
     def get_job_address(self, retry_attempts=10, sleep_interval=30):
         # We retry several times to get job data
         for attempt in range(retry_attempts):
             try:
                 self.update_head_node()
                 if self.head_node is not None:
-                    address=f"http://{self.head_node}:{self.web_port}"
+                    address = f"http://{self.head_node}:{self.web_port}"
                     return address
                 else:
-                    print(f"Attempt {attempt + 1} failed, retrying after {sleep_interval}s...")
+                    print(
+                        f"Attempt {attempt + 1} failed, retrying after {sleep_interval}s..."
+                    )
                     time.sleep(sleep_interval)
             except Exception as e:
                 print(str(e))
 
+
 if __name__ == "__main__":
-    exp = SLURMCluster(job_name="xorbits",num_nodes=2,output_dir="/shared_space/output.out",time="00:30:00")
-    adress = exp.run()
-    print(adress)
+    exp = SLURMCluster(
+        job_name="xorbits",
+        num_nodes=2,
+        output_dir="/shared_space/output.out",
+        time="00:30:00",
+    )
+    address = exp.run()
+    print(address)
     time.sleep(5)
-    adress = "http://c1:16379"
-    xorbits.init(adress)
+    address = "http://c1:16379"
+    xorbits.init(address)
     print(np.random.rand(100, 100).mean())
--- python/xorbits/cluster/test.py
+++ python/xorbits/cluster/test.py
@@ -1,10 +1,9 @@
 import xorbits
 import xorbits.numpy as np
-import time
 
-#exp = SLURMCluster()
-#adress = exp.run()
-#print(adress)
-#time.sleep(5)
+# exp = SLURMCluster()
+# address = exp.run()
+# print(address)
+# time.sleep(5)
 xorbits.init("http://c1:16379")
 print(np.random.rand(5, 5).mean())
