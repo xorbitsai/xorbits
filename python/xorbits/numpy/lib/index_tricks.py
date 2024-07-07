9b536674d00259e4b59bb096f9ce8e22aa39377f
Weizheng Lu <luweizheng36@hotmail.com>
2024-07-07 09:07:48 +0800

    fix import error

--- python/xorbits/numpy/lib/index_tricks.py
+++ python/xorbits/numpy/lib/index_tricks.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import numpy
 from numpy import __version__ as np_ver
 if np_ver >= "2.0.0":
     from numpy.lib import _index_tricks_impl as index_tricks
