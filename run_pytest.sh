e964002624f6df8d3866ebed3117e110de352317
Chao Hui <chaohui6014@gmail.com>
2024-08-30 15:26:25 +0800

    delete helper file

--- run_pytest.sh
+++
@@ -1,5 +0,0 @@
-pytest -s --timeout=1500 \
--k "test_date_range_execution"\
-            -W ignore::PendingDeprecationWarning \
-            --cov-config=setup.cfg --cov-report=xml --cov=xorbits/deploy --cov=xorbits \
-            xorbits/_mars/dataframe/datasource/tests/test_datasource_execution.py > pytest.log 2>&1 
