46a54191097626ee09dcc14d6841afdcd2e751e6
hucorz <chaohui6014@gmail.com>
2024-11-01 15:46:03 +0800

    Support python3.12

--- python/xorbits/_mars/tensor/base/moveaxis.py
+++ python/xorbits/_mars/tensor/base/moveaxis.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from numpy.core.numeric import normalize_axis_tuple
+from numpy._core.numeric import normalize_axis_tuple
 
 from ..datasource import tensor as astensor
 from .transpose import transpose
